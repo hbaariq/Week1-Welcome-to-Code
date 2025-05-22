9567faca15c3553f9124bd6009788a74f721cb78
hbaariq <hafizhbaariq131099@gmail.com>
2025-05-22 22:05:46 +0700

    phase 0 part 2 \

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,61 @@
+soal 1
+
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+//code here
+word = word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth +  ' ' + sixth + ' ' + seventh;
+console.log(word);
+
+soal 2
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2];
+let exampleSecondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13];
+let exampleThirdWord = word[15] + word[16];
+let exampleFourthWord = word[18] + word[19];
+let exampleFifthWord = word[21] + word[22] + word[23] + word[24];
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + exampleSecondWord);
+console.log('Third Word: ' + exampleThirdWord);
+console.log('Fourth Word: ' + exampleFourthWord);
+console.log('Fifth Word: ' + exampleFifthWord);
+
+soal 3
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let exampleSecondWord3 = word3.substring(4, 14);
+let exampleThirdWord3 = word3.substring(15, 17);
+let exampleFourthWord3 = word3.substring(18, 20);
+let exampleFifthWord3 = word3.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + exampleSecondWord3);
+console.log('Third Word: ' + exampleThirdWord3);
+console.log('Fourth Word: ' + exampleFourthWord3);
+console.log('Fifth Word: ' + exampleFifthWord3);
+
+soal 4
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+let exampleSecondWord4 = word4.substring(4, 14);
+let exampleThirdWord4 = word4.substring(15, 17);
+let exampleFourthWord4 = word4.substring(18, 20);
+let exampleFifthWord4 = word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+let secondWordLength = exampleSecondWord4.length;
+let thirdWordLength = exampleThirdWord4.length;
+let fourthWordLength = exampleFourthWord4.length;
+let fifthWordLength = exampleFifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + exampleSecondWord4 + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + exampleThirdWord4 + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + exampleFourthWord4 + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + exampleFifthWord4 + ', with length: ' + fifthWordLength);
